9c46a8d3050017f86b0b9e96e14810cf7335105f
Yan Wong <yan.wong@bdi.ox.ac.uk>
2020-11-19 21:03:35 +0000

    Change to new nexus format

--- ARGmetrics/R/genome_trees_dist.r
+++ ARGmetrics/R/genome_trees_dist.r
@@ -23,8 +23,9 @@
 #' genome.trees.dist()
 
 genome.trees.dist <- function(treeseq.a=NA, treeseq.b=NA, output.full.table = FALSE, acceptable.length.diff.pct = 0.1, variant.positions=NULL, randomly.resolve.a=FALSE, randomly.resolve.b=FALSE, force.rooted=TRUE) { 
-    results=data.frame(unchanged.tree=numeric(), lft=numeric(), rgt=numeric(), RFrooted=numeric(), RFunrooted=numeric(),
-        wRFrooted=numeric(), wRFunrooted=numeric(), SPRunrooted=numeric(), pathunrooted=numeric(), KCrooted=numeric())
+    #results=data.frame(unchanged.tree=numeric(), lft=numeric(), rgt=numeric(), RFrooted=numeric(), RFunrooted=numeric(),
+    #    wRFrooted=numeric(), wRFunrooted=numeric(), SPRunrooted=numeric(), pathunrooted=numeric(), KCrooted=numeric())
+    results=data.frame(unchanged.tree=numeric(), lft=numeric(), rgt=numeric(), RFinfo=numeric())
     
     if (is.na(treeseq.a) || is.na(treeseq.b)) {
         #one or other is null, so simply return the column names for reference
@@ -81,10 +82,10 @@ genome.trees.dist <- function(treeseq.a=NA, treeseq.b=NA, output.full.table = FA
                     message(cond)
                 })
         }
-        brk.a <- as.numeric(names(a))
+        brk.a <- as.numeric(sub('tree[^_]*_(.*)', '\\1', names(a)))
         if (is.unsorted(brk.a))
             stop("Tree names should correspond to numerical breakpoints or variant totals, sorted from low to high, but tree names in the first trees object are not numbers in ascending order.")
-        brk.b <- as.numeric(names(b))
+        brk.b <- as.numeric(sub('tree[^_]*_(.*)', '\\1', names(b)))
         if (is.unsorted(brk.b))
             stop("Tree names should correspond to numerical breakpoints or variant totals, sorted from low to high, but tree names in the second trees object are not numbers in ascending order.")
         if ((max(brk.a) * (100+ acceptable.length.diff.pct)/100 < max(brk.b)) || 
@@ -102,22 +103,26 @@ genome.trees.dist <- function(treeseq.a=NA, treeseq.b=NA, output.full.table = FA
                 break
             }
             rgt <- brk$values[0:1];
-            RFrooted <- RFunrooted <- wRFrooted <- wRFunrooted <- SPRunrooted  <- pathunrooted <- KCrooted  <- NA
-            catchTreeDistErrors({RFrooted <- phangorn::RF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=TRUE)},
+            RFinfo <- NA
+            catchTreeDistErrors({RFinfo <- TreeDist::TreeDistance(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
                 'RF', rooted=TRUE)
-            catchTreeDistErrors({RFunrooted <- phangorn::RF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=FALSE)},
-                'RF')
-            catchTreeDistErrors({wRFrooted <- phangorn::wRF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=TRUE)},
-                'weighted RF', rooted=TRUE)
-            catchTreeDistErrors({wRFunrooted <- phangorn::wRF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=FALSE)},
-                'weighted RF')
-            catchTreeDistErrors({SPRunrooted <- phangorn::SPR.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
-                'subtree prune & regraft')
-            catchTreeDistErrors({pathunrooted <- phangorn::path.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
-                'path distance')
-            catchTreeDistErrors({KCrooted <- kc.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
-                'Kendall-Colijn', rooted=TRUE)
-            results[nrow(results)+1,] <- c(ifelse(length(brk$ind)>1,NA,setdiff(1:2,brk$ind)),lft,rgt,RFrooted, RFunrooted, wRFrooted, wRFunrooted, SPRunrooted, pathunrooted, KCrooted)
+            #catchTreeDistErrors({RFrooted <- phangorn::RF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=TRUE)},
+            #    'RF', rooted=TRUE)
+            #catchTreeDistErrors({RFunrooted <- phangorn::RF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=FALSE)},
+            #    'RF')
+            #catchTreeDistErrors({wRFrooted <- phangorn::wRF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=TRUE)},
+            #    'weighted RF', rooted=TRUE)
+            #catchTreeDistErrors({wRFunrooted <- phangorn::wRF.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]], rooted=FALSE)},
+            #    'weighted RF')
+            #catchTreeDistErrors({SPRunrooted <- phangorn::SPR.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
+            #    'subtree prune & regraft')
+            #catchTreeDistErrors({pathunrooted <- phangorn::path.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
+            #    'path distance')
+            #catchTreeDistErrors({KCrooted <- kc.dist(a[[tree.index.ctr[1]]], b[[tree.index.ctr[2]]])},
+            #    'Kendall-Colijn', rooted=TRUE)
+            #results[nrow(results)+1,] <- c(ifelse(length(brk$ind)>1,NA,setdiff(1:2,brk$ind)),lft,rgt,RFrooted, RFunrooted, wRFrooted, wRFunrooted, SPRunrooted, pathunrooted, KCrooted)
+            #results[nrow(results)+1,] <- c(ifelse(length(brk$ind)>1,NA,setdiff(1:2,brk$ind)),lft,rgt,RFrooted, RFunrooted, wRFrooted, wRFunrooted, SPRunrooted, pathunrooted, KCrooted)
+            results[nrow(results)+1,] <- c(ifelse(length(brk$ind)>1,NA,setdiff(1:2,brk$ind)),lft,rgt,RFinfo)
             lft <- rgt
             tree.index.ctr[brk$ind] <- tree.index.ctr[brk$ind] + 1 #NB, brk$ind is a factor with levels (m1,m2), so we hope that m1==1 and m2==2
         }
@@ -129,12 +134,13 @@ genome.trees.dist <- function(treeseq.a=NA, treeseq.b=NA, output.full.table = FA
             weights <- results$rgt-results$lft
         else
             weights <- tabulate(findInterval(variant.positions, results$rgt, rightmost.closed=TRUE)+1, nbins=nrow(results))
-        return(c(RFrooted=weighted.mean(results$RFrooted,        weights, na.rm = TRUE),
-                 RFunrooted=weighted.mean(results$RFunrooted,    weights, na.rm = TRUE),
-                 wRFrooted=weighted.mean(results$wRFrooted,      weights, na.rm = TRUE),
-                 wRFunrooted=weighted.mean(results$wRFunrooted,  weights, na.rm = TRUE),
-                 SPRunrooted=weighted.mean(results$SPRunrooted,  weights, na.rm = TRUE),
-                 pathunrooted=weighted.mean(results$pathunrooted,weights, na.rm = TRUE),
-                 KCrooted=weighted.mean(results$KCrooted,        weights, na.rm = TRUE)))
+        return(c(RFinfo=weighted.mean(results$RFinfo,        weights, na.rm = TRUE)))
+        #return(c(RFrooted=weighted.mean(results$RFrooted,        weights, na.rm = TRUE),
+        #         RFunrooted=weighted.mean(results$RFunrooted,    weights, na.rm = TRUE),
+        #         wRFrooted=weighted.mean(results$wRFrooted,      weights, na.rm = TRUE),
+        #         wRFunrooted=weighted.mean(results$wRFunrooted,  weights, na.rm = TRUE),
+        #         SPRunrooted=weighted.mean(results$SPRunrooted,  weights, na.rm = TRUE),
+        #         pathunrooted=weighted.mean(results$pathunrooted,weights, na.rm = TRUE),
+        #         KCrooted=weighted.mean(results$KCrooted,        weights, na.rm = TRUE)))
     }
 }
